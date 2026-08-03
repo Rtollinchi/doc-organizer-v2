67d7a5658752398280afa80cd062051fd12cfb33
Rtollinchi <rtollinchi23@gmail.com>
2026-08-03 14:21:08 -0400

    Add tests to validate extract_data does not return an empty list and to validate returns list of strings

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -5,3 +5,7 @@ def test_does_not_return_empty_list():
     result = extract_data("samples/sample.pdf")
     assert isinstance(result, list)
     assert len(result) > 0
+
+def test_returns_list_of_strings():
+    result = extract_data("samples/sample.pdf")
+    assert all(isinstance(item, str) for item in result)
